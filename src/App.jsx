72931f98d0b9b881967cf127e6446028e47cd7ab
jema <smh1141@naver.com>
2026-06-28 22:44:17 +0900

    feat : Router를 이용, 컴포넌트 배치

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
 import "./App.css";
-
+import { Routes, Route } from "react-router";
+import Home from "./pages/Home";
+import Posts from "./pages/Posts";
+import PostNew from "./pages/PostNew";
+import Header from "./components/Header";
+async function postData() {
+  try {
+    const res = await fetch("./data/blog.json");
+    if (!res.ok) throw new Error("로딩에 실패했습니다.");
+    const posts = await res.json();
+    // console.log(posts);
+  } catch (error) {
+    console.log(error);
+  }
+}
+postData();
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/write" element={<PostNew />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
