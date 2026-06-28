9a10110540c4f5cafc9dfcecf2dd64673704fca1
jema <smh1141@naver.com>
2026-06-28 23:37:00 +0900

    feat : outlet 이용 layout Router 설정

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,42 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
 import Home from "./pages/Home";
-import Posts from "./pages/Posts";
-import PostNew from "./pages/PostNew";
-import Header from "./components/Header";
-async function postData() {
-  try {
-    const res = await fetch("./data/blog.json");
-    if (!res.ok) throw new Error("로딩에 실패했습니다.");
-    const posts = await res.json();
-    // console.log(posts);
-  } catch (error) {
-    console.log(error);
-  }
-}
-postData();
+// import Posts from "./pages/Posts";
+import { useEffect, useState } from "react";
+import Layout from "./components/Layout";
+// import PostNew from "./pages/PostNew";
+// import Header from "./components/Header";
+
 function App() {
+  // fetch 조회 완료 유무 : loaded, fetch data 저장 : posts
+  const [loaded, setLoaded] = useState(false);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    async function postData() {
+      try {
+        const res = await fetch("./data/blog.json");
+        if (!res.ok) throw new Error("로딩에 실패했습니다.");
+        const data = await res.json();
+        setPosts(data);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setLoaded(true);
+      }
+    }
+
+    postData();
+  }, []);
+  console.log(posts);
+  console.log(loaded);
+
   return (
     <>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/write" element={<PostNew />} />
+        <Route path="/" element={<Layout loaded={loaded} />}>
+          <Route index element={<Home posts={posts} />} />
+        </Route>
       </Routes>
     </>
   );
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,1 +1,12 @@
-export default function Layout() {}
+import { Outlet } from "react-router";
+import Header from "./Header";
+
+export default function Layout({ loaded }) {
+  // console.log(loaded);
+  return (
+    <div>
+      <Header />
+      {loaded ? <Outlet /> : <p>로딩 중...</p>}
+    </div>
+  );
+}
